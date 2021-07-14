57f38f85135c60c6ee6233395f858a64f517697a
Raashot12 <akannirasheed1996@gmail.com>
2021-07-14 12:34:35 +0100

    Log Fixed

--- config/database.js
+++ config/database.js
@@ -2,37 +2,40 @@ const parse = require('pg-connection-string').parse
 module.exports = ({ env }) => {
   if(env("NODE_ENV")=== "production"){
     const config = parse(process.env.DATABASE_URL);
-    return{
-      defaultConnection: "default",
-      connections:{
-        default:{
-          connector: "bookshelf",
-          settings:{
-            client: "postgres",
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
             host: config.host,
             port: config.port,
             database: config.database,
+            username: config.user,
             password: config.password
           },
           options:{
-            ssl:false
+            ssl: {
+              rejectUnauthorized: false,
+            }
           }
         }
       }
     }
   }
-  return{ defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-}
+  // return{ defaultConnection: 'default',
+  // connections: {
+  //   default: {
+  //     connector: 'bookshelf',
+  //     settings: {
+  //       client: 'sqlite',
+  //       filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  //     },
+  //     options: {
+  //       useNullAsDefault: true,
+  //     },
+  //   },
+  // },
+
 };
