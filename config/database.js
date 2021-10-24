52ebb33451a5ce68a6d46339b4ce1f3e54afc3d9
Raashot12 <akannirasheed1996@gmail.com>
2021-10-24 12:16:41 +0100

    settings support changes made

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const parse = require('pg-connection-string').parse
 module.exports = ({ env }) => {
   if(env("NODE_ENV")=== "production"){
-    const config = parse(process.env.DATABASE_URL);
+    const config = parse( process.env.DATABASE_URL );
+
     return{
       defaultConnection: 'default',
       connections: {
